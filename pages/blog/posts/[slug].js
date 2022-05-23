e73ae6acedc106de32346b627071a30db584ab9a
Fernando Resendiz <resendizbautista801@gmail.com>
2022-05-22 21:43:22 -0500

    Added related posts to blog/posts/[slug]

--- pages/blog/posts/[slug].js
+++ pages/blog/posts/[slug].js
@@ -2,7 +2,8 @@ import { RichText } from '@graphcms/rich-text-react-renderer'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '@/components/Header'
-import { getPostSlugs, getPostData } from '@/lib/api'
+import RelatedPost from '@/components/RelatedPost'
+import { getPostSlugs, getPostData, getPosts } from '@/lib/api'
 
 export async function getStaticPaths() {
   const paths = await getPostSlugs()
@@ -15,19 +16,21 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const post = await getPostData(params.slug)
+  const relatedPosts = (await getPosts()) || []
 
   return {
     props: {
       post,
+      relatedPosts,
     },
   }
 }
 
-export default function Blog({ post }) {
+export default function Blog({ post, relatedPosts }) {
   return (
     <>
       <Header />
-      <div className='relative aspect-video sm:w-full sm:h-72 md:h-80 lg:h-96'>
+      <div className='relative aspect-video sm:h-72 sm:w-full md:h-80 lg:h-96'>
         <Image
           src={post.coverImage.url}
           layout='fill'
@@ -37,7 +40,7 @@ export default function Blog({ post }) {
         />
       </div>
       <section className='overflow-x-hidden'>
-        <div className='container mx-auto xl:max-w-6xl sm:p-8'>
+        <div className='container mx-auto sm:mt-8 sm:px-8 xl:max-w-6xl'>
           <div className='py-12 px-8 sm:px-0'>
             <h2>{post.title}</h2>
             <div className='mt-4'>
@@ -47,6 +50,16 @@ export default function Blog({ post }) {
           </div>
           <div className='rich_text mx-auto flex flex-col gap-6 bg-clay-300 p-8 text-justify'>
             <RichText content={post.content.raw} />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className='container mx-auto py-12 px-8 xl:max-w-6xl'>
+          <p className='text-2xl py-4'>Te Recomendamos:</p>
+          <div className='mx-auto flex mt-4 w-fit max-w-full gap-12 overflow-auto'>
+            {relatedPosts.map((post) => (
+              <RelatedPost key={post.slug} post={post} />
+            ))}
           </div>
         </div>
       </section>
