a56c62ce2fa30fa3f85d16e17d57679da9a2b1fd
Fernando Resendiz <resendizbautista801@gmail.com>
2022-05-30 10:39:04 -0500

    Added Aliados y empresas to paquetes

--- pages/como_ayudar/paquetes.js
+++ pages/como_ayudar/paquetes.js
@@ -14,13 +14,19 @@ export default function Jardines () {
         </article>
       </section>
       <section>
-        <article className='mx-auto container px-4'>
+        <article className='mx-auto container p-4'>
           <h3 className='p-8'>Público en General</h3>
           <div className='rounded-3xl bg-clay-300 p-8 overflow-auto'>
             <PaqueteMonarca title={'El Bicho'}/>
           </div>
         </article>
       </section>
+      <article className='mx-auto container p-4'>
+          <h3 className='p-8'>Aliados y Empresas</h3>
+          <div className='rounded-3xl bg-clay-300 p-8 overflow-auto'>
+            <PaqueteMonarca title={'El Bicho'}/>
+          </div>
+        </article>
     </>
   )
 }
