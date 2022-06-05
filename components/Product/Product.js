7032459659a1173732aef6cf1f10ec9f123aa84c
Fernando Resendiz <resendizbautista801@gmail.com>
2022-06-05 12:18:21 -0500

    Added productos to CMS

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -1,23 +1,26 @@
+import { RichText } from '@graphcms/rich-text-react-renderer'
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function Product({ title, img, price, description, link }) {
+export default function Product({ name, image, price, description, url }) {
   return (
-    <div className='h-full overflow-auto p-6 pb-0 gap-8 grid grid-cols-1 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1 lg:items-center grid-flow-row-dense'>
-      <figure className='relative h-full w-full lg:col-start-2'>
-        <Image src={img} layout='fill' objectFit='contain' alt='bodem' />
+    <div className='h-full overflow-auto p-6 gap-8 grid grid-cols-1 grid-rows-2  grid-flow-row-dense'>
+      <figure className='relative h-full w-full '>
+        <Image src={image.url} layout='fill' objectFit='contain' alt='bodem' />
       </figure>
-      <div className='max-h-full overflow-auto'>
-        <h1 className='max-w-full overflow-auto py-2'>{title}</h1>
+      <div className='max-h-full'>
+        <h1 className='max-w-full overflow-auto py-2'>{name}</h1>
         <h2 className='py-2'>{`$${price}`}</h2>
         <a
-          href={link}
+          href={url}
           target='_blank'
           className='pill my-4 block cursor-pointer px-8 py-2 font-serif text-xl hover:bg-amber-400 active:scale-95'
         >
           Comprar
         </a>
-        <div className='py-2'>{description}</div>
+        <div className='py-2 rich_text flex flex-col gap-4'>
+          <RichText content={description.raw}/>
+        </div>
       </div>
     </div>
   )
--- pages/como_ayudar/productos.js
+++ pages/como_ayudar/productos.js
@@ -4,41 +4,52 @@ import Image from 'next/image'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Product from '@/components/Product'
+import { getProducts } from '@/lib/api'
 
-const products = [
-  {
-    id: '1',
-    title: 'Bodem',
-    img: '/images/products/bodem.png',
-    icon: '/images/products/bodem.png',
-    price: 100.0,
-    description:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quia animi eligendi est suscipit voluptatibus expedita harum optio? Dolor culpa tempore labore, nihil ut cumque vero beatae aut molestias consequatur.',
-    link: 'https://www.mercadolibre.com.mx',
-  },
-  {
-    id: '2',
-    title: 'Paquete Monarca',
-    img: '/images/products/paquete_monarca.png',
-    icon: '/images/products/paquete_monarca.png',
-    price: 200.0,
-    description:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quia animi eligendi est suscipit voluptatibus expedita harum optio? Dolor culpa tempore labore, nihil ut cumque vero beatae aut molestias consequatur.',
-    link: 'https://www.mercadolibre.com.mx',
-  },
-  {
-    id: '3',
-    title: 'Especies Autorizadas',
-    img: '/images/products/especies_autorizadas.png',
-    icon: '/images/products/especies_autorizadas.png',
-    price: 300.0,
-    description:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quia animi eligendi est suscipit voluptatibus expedita harum optio? Dolor culpa tempore labore, nihil ut cumque vero beatae aut molestias consequatur.',
-    link: 'https://www.mercadolibre.com.mx',
-  },
-]
+export async function getStaticProps() {
+  const products = (await getProducts()) || []
 
-export default function Productos() {
+  return {
+    props: {
+      products,
+    },
+  }
+}
+
+// const products = [
+//   {
+//     id: '1',
+//     title: 'Bodem',
+//     img: '/images/products/bodem.png',
+//     icon: '/images/products/bodem.png',
+//     price: 100.0,
+//     description:
+//       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quia animi eligendi est suscipit voluptatibus expedita harum optio? Dolor culpa tempore labore, nihil ut cumque vero beatae aut molestias consequatur.',
+//     link: 'https://www.mercadolibre.com.mx',
+//   },
+//   {
+//     id: '2',
+//     title: 'Paquete Monarca',
+//     img: '/images/products/paquete_monarca.png',
+//     icon: '/images/products/paquete_monarca.png',
+//     price: 200.0,
+//     description:
+//       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quia animi eligendi est suscipit voluptatibus expedita harum optio? Dolor culpa tempore labore, nihil ut cumque vero beatae aut molestias consequatur.',
+//     link: 'https://www.mercadolibre.com.mx',
+//   },
+//   {
+//     id: '3',
+//     title: 'Especies Autorizadas',
+//     img: '/images/products/especies_autorizadas.png',
+//     icon: '/images/products/especies_autorizadas.png',
+//     price: 300.0,
+//     description:
+//       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quia animi eligendi est suscipit voluptatibus expedita harum optio? Dolor culpa tempore labore, nihil ut cumque vero beatae aut molestias consequatur.',
+//     link: 'https://www.mercadolibre.com.mx',
+//   },
+// ]
+
+export default function Productos({ products }) {
   const [selectedProduct, setSelectedProduct] = useState(products[0].id)
 
   const product = products.find((product) => product.id === selectedProduct)
@@ -49,8 +60,8 @@ export default function Productos() {
         <div>
           <Header />
         </div>
-        <div className='container mx-auto grid grid-cols-[100px_1fr] overflow-hidden md:grid-cols-[1fr_4fr] lg:grid-cols-[240px_1fr]'>
-          <div className='flex flex-col items-center justify-center overflow-hidden py-4'>
+        <div className='container mx-auto grid grid-cols-[100px_1fr] overflow-hidden md:grid-cols-[1fr_4fr] lg:grid-cols-[240px_1fr] '>
+          <div className='flex flex-col items-center justify-center overflow-hidden py-6'>
             <div className='h-fit w-full max-w-[200px] overflow-auto rounded-3xl bg-clay-300'>
               {products.map((product) => (
                 <div
@@ -62,9 +73,9 @@ export default function Productos() {
                 >
                   <figure className='relative h-full w-full'>
                     <Image
-                      src={product.icon}
+                      src={product.icon.url}
                       layout='fill'
-                      object-fit='contain'
+                      objectFit='contain'
                     />
                   </figure>
                 </div>
@@ -76,7 +87,7 @@ export default function Productos() {
           </div>
         </div>
       </div>
-      <div className='mt-12'>
+      <div>
         <Footer />
       </div>
     </>
